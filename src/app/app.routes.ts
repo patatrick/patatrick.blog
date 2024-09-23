12ce2ae2a559831bcfd414d8e363218b102efb10
Patrick Romero Figueroa <patrick.cronopolis@yahoo.com>
2024-09-23 20:46:32 -0300

    upd pc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EntradaComponent } from './components/entrada/entrada.component';
 
 export const routes: Routes = [
-    {
-        path:"",
-        component: HomeComponent
-    }
+	{
+		path:"",
+		component: HomeComponent
+	},
+	{
+		path:"entrada",
+		component: EntradaComponent
+	},
 ];
